316f49bde168be969fa151ab15d675bc46c4162a
alepeque9 <alejopequeno25@gmail.com>
2023-05-15 10:42:29 -0300

    2.1

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.use(express.static(publicPath));
 app.listen(8000, () => console.log("Levantando un servidor con Express"));
 
 //pages
+//index
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
+});
+//login
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
 });
