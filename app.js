d3d9f9add6bb46c74c203f1165c7aafefcc088ac
0taky <santiagogcanavide@gmail.com>
2023-05-15 17:21:44 -0300

    2.2

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
+//carrito
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/carrito.html'))
+});
