0fbb36ed59361dfae989a72f642fd5887c7da9f3
0taky <santiagogcanavide@gmail.com>
2023-06-20 22:37:08 -0300

    agregado de tusventas

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -4,5 +4,6 @@ const userController = require('../controller/usersController.js');
 
 router.get("/login", userController.login);
 router.get("/registro", userController.registro);
+router.get("/tusventas", userController.tusventas)
 
 module.exports = router;
