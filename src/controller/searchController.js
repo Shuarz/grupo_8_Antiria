0fc8ddfb02afdf4c4c94104b4c2d47bf8b163f6d
Alejo Pequeño <alejopequeno25@gmail.com>
2023-06-23 19:29:12 -0300

    search

--- src/controller/searchController.js
+++ src/controller/searchController.js
@@ -1,12 +1,21 @@
-const fs = require ('fs');
-const path = require ('path');
-const datos = JSON.parse (fs.readFileSync(path.resolve(__dirname,'../database/product.json')));
+const fs = require('fs');
+const path = require('path');
+const datos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/product.json')));
+
+
 
 
 
 module.exports = {
-    search: (req, res) =>{
-        let search = datos.filter((row) => row.categoria == req.query.search);
-        return res.render('./search/search', {search: search});
+    search: (req, res) => {
+        let search = datos.filter((row) => {
+            const categoria = (row.categoria || '').toString().toLowerCase();
+            const descIndex = (row.descIndex || '').toString().toLowerCase();
+            const query = (req.query.search || '').toString().toLowerCase();
+
+            return categoria.includes(query) || descIndex.includes(query);
+        });
+
+        return res.render('./search/search', { search: search });
     }
 };
