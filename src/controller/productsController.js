c0d4fe10118bf18ccf6b22c8bf75ac71fde1b538
0taky <santiagogcanavide@gmail.com>
2023-06-24 17:06:24 -0300

    crud vender a la mitad

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,10 +1,12 @@
 const fs = require ('fs');
 const path = require ('path');
+const rutaproducto = path.resolve('./src/database/product.json')
 const datos = JSON.parse (fs.readFileSync (path.resolve(__dirname,'../database/product.json')));
 
 module.exports = {
     carrito: (req, res) =>{
         return res.render('../views/products/carrito.ejs' );
+
     },
     detalleProducto:  (req,res)=>{
             const productoEncontrado = datos.find(row => row.id == req.params.id)
@@ -15,6 +17,24 @@ module.exports = {
             "descGeneral":productoEncontrado.descGeneral})
     },
     vender: (req, res) =>{
-        return res.render('../views/products/vender.ejs' );
+        
+        return res.render('../views/products/vender.ejs');
+        
+        
+    },
+
+    publicado:(req, res)=>{
+        let productoNuevo ={
+            "id": datos.length+1,
+            "nombreProd": req.body.nombreProducto,
+            "Precio": req.body.precioProducto,
+            // agregar oferta
+            "categoria": req.body.categoriaProducto,
+            "descGeneral": req.body.descripcionGeneral,
+            "image": req.body.imagenProducto,
+        }
+        fs.writeFileSync(rutaproducto, JSON.stringify([...datos, productoNuevo], null ,2 ), "utf-8")
+        res.send("producto subido!")
+
     }
 };
--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+
 const Controller = require('../controller/productsController.js');
+const multer= require('multer');
+const path = require('path')
+const storage = multer.diskStorage({
+    destination:(req, res ,cb)=>[
+        cb(null, path.join(__dirname,'../public/img'))
+    ],
+    filename:(req,file,cd)=>{
+        console.log(file)
 
+        const newfilename = ""; 
+        cd(null, newfilename)
+    }
+});
 
+const upload = multer({storage});
 router.get("/carrito", Controller.carrito);
 router.get("/productDetail/:id", Controller.detalleProducto);
 router.get ("/vender", Controller.vender);
+router.post("/vender", upload.single("imagenProducto") ,Controller.publicado);
 
 module.exports = router;
