935fd2fab66766a3b0e29e14e24c47c5a502141a
Alejo Pequeño <alejopequeno25@gmail.com>
2023-09-04 15:43:05 -0300

    dark mode terminado

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,39 +1,36 @@
 const toggleBtn = document.getElementById('toggleBtn');
-const header = document.querySelector('header');
-const menuItems = document.querySelectorAll('.link-nav-index');
-var logo = document.getElementById("logoAntiria");
 const body = document.body;
+var logo = document.getElementById("logoAntiria");
 
-
-// Define la ruta de la imagen por defecto y la personalizada
+// Define la ruta del logo por defecto y la personalizada
 var imagenDefault = "/img/logotipo.png";
 var imagenPersonalizada = "/img/logotipo_dark.png";
 
-// Variable para llevar un registro del estado actual
-var imagenActual = "default";
+// Función para aplicar el modo oscuro
+function activarModoOscuro() {
+    body.classList.add('dark-mode');
+    logo.src = imagenPersonalizada;
+    localStorage.setItem('modoOscuro', 'true');
+}
+
+// Función para desactivar el modo oscuro
+function desactivarModoOscuro() {
+    body.classList.remove('dark-mode');
+    logo.src = imagenDefault;
+    localStorage.setItem('modoOscuro', 'false');
+}
+
+// Verificar si el modo oscuro estaba activado en el localStorage
+const modoOscuroAlmacenado = localStorage.getItem('modoOscuro');
+
+if (modoOscuroAlmacenado === 'true') {
+    activarModoOscuro();
+}
 
 toggleBtn.addEventListener('click', () => {
-    //dark mode
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('header-dark');
-    if (imagenActual === "default") {
-        logo.src = imagenPersonalizada;
-        imagenActual = "personalizada";
-      } else {
-        logo.src = imagenDefault;
-        imagenActual = "default";
-      }
-
-    //light mode
-    body.classList.toggle('light-mode');
-    header.classList.toggle('header-index');
-
-
-    menuItems.forEach(item => {
-        item.classList.toggle('link-nav-dark');
-    });
-
-    menuItems.forEach(item => {
-        item.classList.toggle('link-nav-index');
-    });
+    if (body.classList.contains('dark-mode')) {
+        desactivarModoOscuro();
+    } else {
+        activarModoOscuro();
+    }
 });
