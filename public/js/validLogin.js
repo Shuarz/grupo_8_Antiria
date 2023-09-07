5378e3dc8861b2a7b03ee008674bff15ec553963
Santiago Sanchez <s.sanchez2002@hotmail.com>
2023-09-06 23:43:33 -0300

    validacion de formulario hecha y funcional, restan detalles

---
+++ public/js/validLogin.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+    let formulario = document.querySelector('.validForm')
+    console.log(formulario)
+
+    formulario.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        let errores = [];
+
+        const mail = document.querySelector('#mail')
+
+        if (mail.value == '') {
+            errores.push('el campo email debe estar completo ')
+
+        }
+        
+
+        const contra = document.querySelector('#contraseña')
+        if (contra.value == '') {
+            errores.push('el campo contraseña debe estar completo. ')
+
+        } else if (contra.length < 5) {
+            errores.push('el campo debe tener al menos 5 caracteres')
+        }
+
+        if (errores.length > 0) {
+            e.preventDefault();
+
+            let ulErrores = document.querySelector('#listaErrores')
+
+            for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += "<ul>" + errores[i] + "</ul>"
+
+            }
+        }
+
+
+
+    })
+}
+
+
+
